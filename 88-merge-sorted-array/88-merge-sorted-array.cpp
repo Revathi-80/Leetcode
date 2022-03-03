48a3f8e6a5b13af628bcd62e9c5ff6dc49b78360
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-03-03 18:53:58 +0530

    Time: 0 ms (100.00%), Space: 9 MB (73.09%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i=m-1,j=n-1,k=m+n-1;
+    while(i>=0 && j>=0){
+        if(nums1[i]>nums2[j])
+            nums1[k--]=nums1[i--]; // the greatest among the two is inserted at the end of nums1
+        else
+            nums1[k--]=nums2[j--];
+    }
+    // the remainig elements are added to nums1
+        while(i>=0)
+            nums1[k--]=nums1[i--];
+        while(j>=0)
+            nums1[k--]=nums2[j--];
+    }
+};
