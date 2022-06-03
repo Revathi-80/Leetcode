4658159a2d7b3b96d98259b5624f69d8ab04d7ff
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-06-03 22:02:09 +0530

    Time: 14 ms (84.91%), Space: 13.1 MB (96.00%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,21 +2,31 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size(); 
-        set<int>rows;
-        set<int>cols;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    rows.insert(i);
-                    cols.insert(j);
-                }
-            }
+        bool set=false;
+       for(int i=0;i<n;i++){
+           if(matrix[i][0]==0)
+               set=true;
+           for(int j=1;j<m;j++){
+               if(matrix[i][j]==0){
+                   matrix[0][j]=0; 
+                   matrix[i][0]=0;
+               }
+           }
+       }
+       for(int i=1;i<n;i++){
+           for(int j=1;j<m;j++){
+               if(matrix[i][0]==0 || matrix[0][j]==0){
+                   matrix[i][j]=0;
+               }
+           }
+       }
+        if(matrix[0][0]==0) {  // due to first row
+           for(int j=0;j<m;j++)
+               matrix[0][j]=0;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(rows.find(i)!=rows.end() || cols.find(j)!=cols.end() )
-                    matrix[i][j]=0;
-            }
+        if(set){           // due to the first column
+         for(int i=0;i<n;i++)
+               matrix[i][0]=0;   
         }
     }
 };
