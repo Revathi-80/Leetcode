415acdcfc55b3d5f033c2449c963b83b54a97da7
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-06-09 10:20:06 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (34.06%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-        for (int i=0;i<(n+1)/2;i++){
-            for(int j=0;j<n/2;j++){
-                int temp=matrix[n-j-1][i];
-                matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
-                matrix[j][n-i-1]=matrix[i][j];
+        transpose(matrix);
+        ref(matrix);
+    }
+    void transpose(vector<vector<int>>&matrix){
+        int n=matrix.size();
+        for(int i=0;i<(n);i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[j][i];
+                matrix[j][i]=matrix[i][j];
                 matrix[i][j]=temp;
             }
         }
     }
+    void ref(vector<vector<int>>&matrix){
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][n-j-1];
+                matrix[i][n-j-1]=matrix[i][j];
+                matrix[i][j]=temp;
+            }
+        }
+    }
+    
+    
 };
