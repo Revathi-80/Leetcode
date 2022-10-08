0f8d56e20335402fbedcf4b0fbfa7652826343e9
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-10-08 16:39:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.27%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev=1,prev2=1,curr;
+        for(int i=2;i<=n;i++){
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
