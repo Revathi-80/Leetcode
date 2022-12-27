a120133029fa8563a273b43ccb8d0473aafcc2c0
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-12-27 08:45:46 +0530

    Time: 164 ms (58.47%), Space: 54.8 MB (12.84%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       int n=nums.size();
+        set<int>s;
+        for(int i=0;i<n;i++) {
+            s.insert(nums[i]);
+        }
+        if(s.size()!=n)
+            return true;
+        else
+            return false;
+    }
+};
