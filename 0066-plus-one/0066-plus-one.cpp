d2228de1eacdff2fe4e0eb4b1dbd847f90804614
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-12-28 07:52:05 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (87.22%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n=digits.size()-1;
-       for(int i=n;i>=0;i--){
-          if(digits[i]==9)
-              digits[i]=0;
-           else{
-               digits[i]+=1;
-               return digits;
-           }
-           
-       }
-        digits.push_back(0);
-        digits[0]=1;
+       int n=digits.size();
+        int carry=0;
+        bool flag=false;
+        if(digits[n-1]==9) {
+            digits[n-1]=0;
+            carry=1;
+            flag=true;
+        }
+        else {
+            digits[n-1]++;
+        }
+        for(int i=n-2;i>=0;i--) {
+          if(flag){  
+            if(digits[i]==9) {
+                carry=1;
+                digits[i]=0;
+                flag=true;
+            }
+            else {
+                digits[i]=digits[i]+carry;
+                carry=0;
+                flag=false;
+            }
+          }
+        }
+        if(carry==1) {
+            reverse(digits.begin(),digits.end());
+            digits.push_back(1);
+            reverse(digits.begin(),digits.end());
+        }
         return digits;
     }
 };
