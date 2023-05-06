2ea33a9f73b9c0142fdbe3c11f6a22ef1e4df889
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-06 08:45:07 +0530

    Time: 33 ms (16.18%), Space: 19.3 MB (61.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        vector<int>temp;
+        int j=-1;
         for(int i=0;i<n;i++) {
-            if(nums[i]!=0)
-                temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
         }
-        int k= temp.size();
-        for(int i=0;i<k;i++) {
-            nums[i]=temp[i];
-        }
-        for(int i=k;i<n;i++) {
-            nums[i]=0;
+        if(j==-1) j=n;
+        for(int i=j+1;i<n;i++) {
+            if(nums[i]!=0){
+            swap(nums[j],nums[i]);
+                j++;
+            }
         }
     }
 };
+
+
+// brute force
+// int n=nums.size();
+//         vector<int>temp;
+//         for(int i=0;i<n;i++) {        //O(n)
+//             if(nums[i]!=0)
+//                 temp.push_back(nums[i]);
+//         }
+//         int k= temp.size();
+//         for(int i=0;i<k;i++) {
+//             nums[i]=temp[i];
+//         }
+//         for(int i=k;i<n;i++) {
+//             nums[i]=0;
+//         }
