1d3791e0d3255baad49a13a15ce980348059bf54
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-07 12:39:06 +0530

    Time: 27 ms (34.83%), Space: 20.9 MB (5.46%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
+        int maxi=0;
+        unordered_map<long long ,int>hash;
         for(int i=0;i<n;i++) {
-            int x=nums[i];
-            int cnt=0;
-            for(int j=0;j<n;j++) {
-                if(nums[j]==x)
-                    cnt++;
-            }
-            if(cnt==1)
-                return nums[i];
+            hash[nums[i]]++;
+        }
+        for(auto it :hash) {
+            if(it.second==1)
+                return it.first;
         }
         return 0;
     }
