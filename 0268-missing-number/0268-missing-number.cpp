37834bbde97dfe8f2bafd187c1e8f3fa6510bafb
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-07 12:15:02 +0530

    Time: 11 ms (98.16%), Space: 17.9 MB (65.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,11 +3,10 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int xor1=0,xor2=0;
-        for(int i=0;i<=n;i++) {
-            xor1^=i;
-        }
+    
         for(int i=0;i<n;i++) {
             xor2^=nums[i];
+            xor1^=(i+1);
         }
         
         return xor1^xor2;
