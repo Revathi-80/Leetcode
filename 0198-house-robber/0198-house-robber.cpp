7e317d13534705dfbd9f88bfce7b5767c339a8c1
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-15 23:13:27 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (31.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,7 +11,14 @@ public:
     int rob(vector<int>& nums) {
          int n=nums.size();
         vector<int>dp(n+1,-1);
-       
-        return maxi(nums,n-1,dp);
+         dp[0]=nums[0];
+        for(int i=1;i<n;i++) {
+            int pick=nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            int not_pick=dp[i-1];
+            dp[i]=max(pick,not_pick);
+        }
+        return dp[n-1];
     }
 };
