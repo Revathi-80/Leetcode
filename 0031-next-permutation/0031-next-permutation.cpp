2efb0748d1da464820cbf990fd4004e2ed5531d5
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-17 22:27:39 +0530

    Time: 3 ms (82.21%), Space: 12 MB (93.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       int n=nums.size();
-        // if(n==1) return;
-        int k,i,ind;
-        for( k=n-2;k>=0;k--) {
-            if(nums[k]<nums[k+1]) {
-                break;
-            }
-        }
-        if(k<0 ) {
-            reverse(nums.begin(),nums.end());
-        }
-        else {
-     
-        for(i=n-1;i>=0;i--) {
-            if(nums[i]>nums[k]) {
+        int n=nums.size();
+        //longest common prefix---find the break point
+        int ind=-1;
+        for(int i=n-2;i>=0;i--) {
+            if(nums[i]<nums[i+1]) {
                 ind=i;
                 break;
             }
         }
-          // cout <<ind<<" "<<k;  
-        swap(nums[k],nums[ind]);
-        reverse(nums.begin()+k+1,nums.end());
+        if(ind==-1) {
+            reverse(nums.begin(),nums.end());
+            return;
         }
+        // smallest which juuuuuust greater than nums[ind]
+        for(int i=n-1;i>=ind;i--) {
+            if(nums[ind]<nums[i]) {
+                swap(nums[ind],nums[i]);
+                break;
+            }
+        }
+        //smallest possible permutation
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
