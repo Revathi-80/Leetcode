6963143e555e6aa491567b36631a8882782c6024
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-19 23:14:27 +0530

    Time: 10 ms (93.65%), Space: 13.3 MB (68.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,24 +4,33 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row=matrix.size();
         int col=matrix[0].size();
-        vector<int>rows(row,0);
-        vector<int>cols(col,0);
+        int col0=1;
         for(int i=0;i<row;i++) {
             for(int j=0;j<col;j++){
                 if(matrix[i][j]==0){
-                    rows[i]=1;
-                    cols[j]=1;
+                    matrix[i][0]=0; //rows
+                    if(j!=0)
+                    matrix[0][j]=0;
+                    else 
+                        col0=0;
+                    
                 }
             }
         }
-        for(int i=0;i<row;i++) {
-            for(int j=0;j<col;j++) {
+        for(int i=1;i<row;i++) {
+            for(int j=1;j<col;j++) {
                 if(matrix[i][j]!=0) {
-                    if(rows[i]==1 || cols[j]==1) {
+                    if(matrix[i][0]==0 || matrix[0][j]==0) {
                         matrix[i][j]=0;
                     }
                 }
             }
+        }
+        if(matrix[0][0]==0) {
+            for(int j=0;j<col;j++) matrix[0][j]=0;
+        }
+        if(col0==0) {
+            for(int i=0;i<row;i++) matrix[i][0]=0;
         }
     }
 };
