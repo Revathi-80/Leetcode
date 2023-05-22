043c12c4f2e619489c06661e058ac487e80a5710
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-05-22 14:07:46 +0530

    Time: 30 ms (39.97%), Space: 13.4 MB (92.90%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,21 +1,16 @@
 class Solution {
 private:
-    bool check(int start,vector<vector<int>>& graph,int color[]){
-        queue<int>q;
-        q.push(start);
-        color[start]=0;
-        while(!q.empty()) {
-            int node=q.front();
-            q.pop();
-            for(auto it: graph[node]){
+    bool check(int start,vector<vector<int>>& graph,int color[],int col){
+       
+        color[start]=col;
+        
+            for(auto it: graph[start]){
                 if(color[it]==-1) {
-                    color[it]=!color[node];
-                    q.push(it);
+                    if(check(it,graph,color,!col)==false) return false;
                 }
-                else if(color[it]==color[node])
+                else if(color[it]==col)
                     return false;
             }
-        }
         return true;
     }    
 public:
@@ -25,7 +20,7 @@ public:
         for(int i=0;i<n;i++) color[i]=-1;
         for(int i=0;i<n;i++) {
             if(color[i]==-1) {
-                if(check(i,graph,color)==false)
+                if(check(i,graph,color,0)==false)
                     return false;
             }
         }
