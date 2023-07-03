5b328d04c834ab9cf08943575416c23f84dbc91b
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-07-03 07:35:54 +0530

    Time: 32 ms (14.11%), Space: 15.4 MB (83.97%) - LeetHub

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestRangeII(vector<int>& nums, int k) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans=nums[n-1]-nums[0];
+        for(int i=0;i<n-1;i++){
+            int maxi= max(nums[n-1]-k,nums[i]+k);
+            int mini=min(nums[0]+k,nums[i+1]-k);
+            ans=min(ans,maxi-mini);
+        }
+        return ans;
+    }
+};
