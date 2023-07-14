81b0022e3b0b65928eea93d8e0111bb8ac76d26c
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-07-14 19:27:48 +0530

    Time: 45 ms (45.05%), Space: 19.1 MB (21.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,20 +1,21 @@
 class Solution {
-    void bfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&vis){
+private:
+    void bfs(vector<vector<char>>& grid,vector<vector<int>>&vis, int row,int col){
+        vis[row][col]=1;
         int n=grid.size();
         int m=grid[0].size();
-        vis[row][col]=1;
         queue<pair<int,int>>q;
         q.push({row,col});
         while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
+            int srow= q.front().first;
+            int scol=q.front().second;
             q.pop();
-            int arr[]={0,-1,0,1,0};
-            for(int i=0;i<4;i++) {
-                int nrow=row+arr[i];
-                int ncol=col+arr[i+1];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] 
-                  && grid[nrow][ncol]=='1') {
+            int delrow[]={-1,0,1,0};
+            int delcol[]={0,1,0,-1};
+            for(int i=0;i<4;i++){
+                int nrow= srow+delrow[i];
+                int ncol = scol+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
                     vis[nrow][ncol]=1;
                     q.push({nrow,ncol});
                 }
@@ -27,11 +28,11 @@ public:
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         int cnt=0;
-        for(int i=0;i<n;i++) {
-            for (int j=0;j<m;j++) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j]=='1') {
                     cnt++;
-                    bfs(i,j,grid,vis);
+                    bfs(grid,vis,i,j);
                 }
             }
         }
