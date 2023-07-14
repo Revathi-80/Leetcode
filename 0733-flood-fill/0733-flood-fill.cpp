ef07ccb938cdf4ec252c68259437cd3e96163eb7
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-07-14 19:15:08 +0530

    Time: 12 ms (41.04%), Space: 14.1 MB (50.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,27 @@
 class Solution {
 private:
-    void dfs(int row,int col,int color,int iniColor,vector<vector<int>>&ans, vector<vector<int>>&image, int dr[],int dc[] ){
-        ans[row][col]=color;
+    void dfs(vector<vector<int>>& image,vector<vector<int>>&vis, int row,int col, int color ,int ini){
+        vis[row][col]=1;
+        image[row][col]=color;
         int n=image.size();
         int m=image[0].size();
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
         for(int i=0;i<4;i++){
-            int nrow=row+dr[i];
-            int ncol=col+dc[i];
-            if(ncol>=0 && ncol<m && nrow>=0 && nrow<n && ans[nrow][ncol]!=color && image[nrow][ncol]==iniColor){
-                dfs(nrow,ncol,color,iniColor,ans,image,dr,dc);
+            int nrow=row+delrow[i];
+            int ncol = col+delcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==ini && !vis[nrow][ncol]) {
+                dfs(image,vis,nrow,ncol,color,ini);
             }
         }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-         int iniColor=image[sr][sc]; 
-      int n=image.size();
-      int m=image[0].size();
-      vector<vector<int>>ans=image;
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-      dfs(sr,sc,color,iniColor,ans,image,dr,dc);
-        return ans;
-        
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int ini=image[sr][sc];
+        dfs(image,vis,sr,sc,color,ini);
+        return image;
     }
 };
