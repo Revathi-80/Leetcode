07b2813cc950687d25dafbaef17af36ae6518042
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-07-15 08:12:11 +0530

    Time: 4 ms (13.73%), Space: 6.1 MB (36.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    int func(int n,vector<int>&dp){
+        if(n<=1) return dp[n]=1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=func(n-1,dp)+func(n-2,dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return func(n,dp);
+    }
+};
