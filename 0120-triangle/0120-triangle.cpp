d1cd2b89f25872abd5b2498a460a9517e3b1c541
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-08-09 22:54:11 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (67.84%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,18 @@
 class Solution {
-    int paths (vector<vector<int>>&triangle, int i, int j,int n,vector<vector<int>>&dp) {
-       
-        if(i == n-1) return triangle[n-1][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down= triangle[i][j] + paths(triangle,i+1,j,n,dp);
-        int diagonal=triangle[i][j] +paths(triangle,i+1,j+1,n,dp);
-        return dp[i][j]=min(down,diagonal);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size(),m=triangle[n-1].size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int j=0;j<m;j++){
-            dp[n-1][j]=triangle[n-1][j];
+        int n=triangle.size();
+        vector<int>front(n);
+        for(int j=0;j<n;j++){
+            front[j]=triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--) {
+            vector<int>curr(n);
             for(int j=i;j>=0;j--) {
-                dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+                curr[j]=triangle[i][j]+min(front[j],front[j+1]);
             }
+            front=curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
