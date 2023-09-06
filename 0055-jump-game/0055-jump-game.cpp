e44081ea09ea7f0561979d5f81ca2661f49fcb9b
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-09-06 20:15:36 +0530

    Time: 37 ms (96.27%), Space: 48.4 MB (77.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int max_reach=0; //pink pointer
-        for(int i=0;i<n;i++) { //red pointer
-            // red pointer < pink pointer 
-            // update the max position of pink pointer
-            if(i<=max_reach) {
-                max_reach = max(max_reach , i+nums[i]);
-            }
-            else {
+        int reached=0;
+        for(int i=0;i<n;i++) {
+            if(reached<i) 
                 return false;
-            }
+            reached= max(reached,i+nums[i]);
         }
         return true;
     }
