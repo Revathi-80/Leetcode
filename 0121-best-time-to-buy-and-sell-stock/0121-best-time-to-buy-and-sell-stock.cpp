217982ecd32774ae4ad7d0bde00a48168b19971c
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-09-28 09:09:34 +0530

    Time: 100 ms (33.66%), Space: 93.8 MB (10.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,7 @@ public:
         int n=prices.size();
         int s=prices[0],profit=0;
         for(int i=1;i<n;i++) {
-            if(prices[i]<s) {
-                s=prices[i];
-            }
+            s=min(s,prices[i]);
             profit=max(profit,prices[i]-s);
         }
         return profit;
