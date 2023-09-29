1eacb198bbdc95725a3813fc278610c1dd2f5a3a
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-09-29 09:24:37 +0530

    Time: 287 ms (58.94%), Space: 133.2 MB (60.65%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,41 +1,20 @@
 class Solution {
-    int f(vector<int>&prices,int ind,int buy,int n, vector<vector<vector<int>>>&dp,int cnt) {
-        if(ind==n || cnt==0 ) return 0;
-        if(dp[ind][buy][cnt]!=-1) return dp[ind][buy][cnt];
-        int profit=0;
-        if(buy) {
-            profit = max(-prices[ind]+ f(prices,ind+1,0,n,dp,cnt),
-                        0 + f(prices,ind+1,1,n,dp,cnt));
+    int f(vector<int>&prices,int n,int ind,int tran ,vector<vector<int>>&dp) {
+        if(ind==n || tran==4) return 0;
+        if(dp[ind][tran]!=-1) return dp[ind][tran];
+        if(tran%2==0) {
+            return dp[ind][tran]=max(-prices[ind] +f(prices,n,ind+1,tran+1,dp),
+                       0 + f(prices,n,ind+1,tran,dp));
         }
         else {
-            profit= max( prices[ind] + f(prices,ind+1,1,n,dp,cnt-1),
-                       0 + f(prices,ind+1,0,n,dp,cnt));
+            return dp[ind][tran]=max(prices[ind] + f(prices,n,ind+1,tran+1,dp),
+                       0 + f(prices,n,ind+1,tran,dp));
         }
-        return dp[ind][buy][cnt]=profit;
     }
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>ahead(2,vector<int>(3,0));
-         vector<vector<int>>curr(2,vector<int>(3,0));
-        
-        for(int ind=n-1;ind>=0;ind--) {
-            for(int buy=0;buy<=1;buy++) {
-                for(int cnt=1;cnt<=2;cnt++) {
-                    if(buy) {
-                        curr[buy][cnt] = max( -prices[ind] + ahead[0][cnt],
-                                    0 + ahead[1][cnt]);
-                    }
-                    else {
-                        curr[buy][cnt] = max(prices[ind] + ahead[1][cnt-1],
-                                    0 + ahead[0][cnt]);
-                        
-                    }
-                }
-            }
-            ahead=curr;
-        }
-        
-        return ahead[1][2];
+        int n= prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(4,-1));
+        return f(prices,n,0,0,dp);
     }
 };
