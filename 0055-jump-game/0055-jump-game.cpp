0b4d194c315d54a76e1039ba9d5c69297fd84f2a
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-09-30 09:58:27 +0530

    Time: 46 ms (68.84%), Space: 48.6 MB (38.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,10 +4,10 @@ public:
         int n=nums.size();
         int maxi=0;
         for(int i=0;i<n;i++) {
-            if(maxi>=i) {
-                maxi=max(maxi,i+nums[i]);
-            }
+            if(maxi<i) 
+                return false;
+            maxi=max(maxi,i+nums[i]);
         }
-        return maxi>=n-1;
+        return true;
     }
 };
